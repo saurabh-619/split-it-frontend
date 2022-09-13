5927a0e2a31244097f0b83b05d65654579f77089
Saurabh Bomble <saurabhbomble107@gmail.com>
2022-09-14 00:04:23 +0530

    feat: init iOS project with MVVM arch

---
+++ Frontend/Core/Login/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Frontend
+//
+//  Created by Saurabh Bomble on 14/09/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
--- Frontend/FrontendApp.swift
+++ Frontend/FrontendApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FrontendApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
